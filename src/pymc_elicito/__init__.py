6a90739e4518161ab700c729da276c56cde0f71d
florence-bockting <florence.bockting@tu-dortmund.de>
2025-11-21 10:35:43 +0100

    raw package template

---
+++ src/pymc_elicito/__init__.py
@@ -0,0 +1,7 @@
+"""
+Learning prior distributions for model parameters in a Bayesian model based on expert information.
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("pymc_elicito")
