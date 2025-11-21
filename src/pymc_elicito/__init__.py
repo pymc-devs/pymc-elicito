fcf171707afb7d81a9fd39c8ef85be599f9039d2
florence-bockting <florence.bockting@tu-dortmund.de>
2025-11-21 12:48:07 +0100

    add test function

--- src/pymc_elicito/__init__.py
+++ src/pymc_elicito/__init__.py
@@ -1,7 +1,24 @@
 """
-Learning prior distributions for model parameters in a Bayesian model based on expert information.
+A framework for expert prior elicitation in Python.
+
+Learn prior distributions for parameters in a Bayesian
+model based on expert information.
 """
 
 import importlib.metadata
 
 __version__ = importlib.metadata.version("pymc_elicito")
+
+__all__ = ["hello_world"]
+
+
+def hello_world() -> str:
+    """
+    Print hello world
+
+    Returns
+    -------
+    :
+        hello world
+    """
+    return "hello world"
